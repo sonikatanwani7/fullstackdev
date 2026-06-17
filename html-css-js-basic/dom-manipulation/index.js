d2a0371957fd143c286849c58fa9f90c2db09342
sonikatanwani7 <sonikatanwani7@gmail.com>
2026-06-17 12:53:08 +0530

    getElementsByClassName added

--- html-css-js-basic/dom-manipulation/index.js
+++ html-css-js-basic/dom-manipulation/index.js
@@ -11,3 +11,11 @@ const basketHeading = document.getElementById('basket-heading')
 basketHeading.style.color = "green"
 
 document.getElementById('thanks').innerHTML = '<p>Please visit us again</p>'
+
+const fruit = document.getElementsByClassName('fruit');
+
+fruit[2].style.backgroundColor = "yellow";
+
+for (let i = 0; i < fruit.length; i++){
+    fruit[i].style.fontWeight = "bold"
+}
